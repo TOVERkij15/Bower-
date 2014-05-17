3f9073bbf2e6473ac742b1c108bddaba9b905d35
tasha kijsamnong <tkijsamnong@yahoo.com>
2014-05-17 17:07:58 -0400

    process

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,55 +14,21 @@ function renderRepos(repos){
 }
 
 
+var gitHubProfile = _.template($('.git-hub-profile').text());
+
+
+$.getJSON("https://api.github.com/users/TOVERkij15?client-id=31b6368691d0774c212b&client-secret=227508046ccf4d97d53a93dabb0a06d6a3eedd9f").done(function(profile){
+    renderProfile(profile)
+})
+  function renderProfile(profile){
+
+    var showRepos = _.template($(".get-info").text());
+
+    var rendered = showRepos(profile);
+
+    $('.column-one').prepend(rendered);
+  };
 
 
 
 
-
-/*var getName = _.template($(".get-name").text());
-
-repos.forEach(function(name) {
-
-var allRepos = getName(name);
-
-  $(".column-two").prepend(allRepos);
-});
-
-
-var getStar = _.template($('.get-star').text());
-
-repos.forEach(function(stargazers_count) {
-
-  var allRepos = getStar(stargazers_count);
-
-  $(".column-two").prepend(allRepos);
-});
-
-var getforks = _.template($('.get-forks').text());
-
-repos.forEach(function(forks_count) {
-
-  var allRepos = getforks(forks_count);
-
-  $(".column-two").prepend(allRepos);
-});
-
-
-var getlanguage = _.template($('.get-language').text());
-
-repos.forEach(function(language_count) {
-
-  var allRepos = getlanguage(language_count);
-
-  $(".column-two").prepend(allRepos);
-});
-
-var getdescription = _.template($('.get-description').text());
-
-repos.forEach(function(description) {
-
-  var allRepos = getdescription(description);
-
-  $(".column-two").prepend(allRepos);
-});
-*/
